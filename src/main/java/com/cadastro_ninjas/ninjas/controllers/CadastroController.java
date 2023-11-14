83e2ea5ac223fb5d50f67c49678824b7569352be
IsabelaBaseggio <isabela.baseggio7@gmail.com>
2023-11-14 16:33:47 -0300

    Get listaNinjas e listaNinjaUnico

--- src/main/java/com/cadastro_ninjas/ninjas/controllers/CadastroController.java
+++ src/main/java/com/cadastro_ninjas/ninjas/controllers/CadastroController.java
@@ -1,13 +1,33 @@
 package com.cadastro_ninjas.ninjas.controllers;
 
+import com.cadastro_ninjas.ninjas.models.NinjasModel;
+import com.cadastro_ninjas.ninjas.repository.MissaoRepository;
+import com.cadastro_ninjas.ninjas.repository.NinjasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/")
 public class CadastroController {
+    @Autowired
+    NinjasRepository ninjasRepository;
+
+    @Autowired
+    MissaoRepository missaoRepository;
     @GetMapping("/ninjas")
+    public List<NinjasModel> listaNinjas (){
+        return ninjasRepository.findAll();
+    }
+
+    @GetMapping("/ninja/{id}")
+    public NinjasModel listaNinjaUnico(@PathVariable(value = "id") long id){
+        return ninjasRepository.findById(id);
+    }
 
 
 
--- src/main/java/com/cadastro_ninjas/ninjas/repository/NinjasRepository.java
+++ src/main/java/com/cadastro_ninjas/ninjas/repository/NinjasRepository.java
@@ -4,5 +4,5 @@ import com.cadastro_ninjas.ninjas.models.NinjasModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NinjasRepository extends JpaRepository<NinjasModel, Long> {
-
+    NinjasModel findById(long id);
 }
